9ddc173e22c3b6c6747d6459837a4c3d2c968c3b
Fangshu Ye <56810803+fangshuye@users.noreply.github.com>
2022-10-20 09:37:07 -0500

    Add files via upload

---
+++ Rshiny/SolveSampleSize.R
@@ -0,0 +1,204 @@
+packages <- c("MASS","NlcOptim","netmeta","dplyr")
+lapply(packages, library, character.only = TRUE)
+
+SolveSampleSize_Withprev <- function(p_CEFTP,p_TILD_c,sigma,power_level){
+  beta1 = log(p_CEFTP/(1-p_CEFTP))
+  beta2 = log(p_TILD_c/(1-p_TILD_c)) - log(p_CEFTP/(1-p_CEFTP))
+  mu_1 <- exp(beta1)/(1+exp(beta1))^2
+  mu_2 <- exp(beta1 + beta2)/(1+exp(beta1 + beta2))^2
+  sigma_prev <- sigma
+  n0=c(1,1)
+  
+  power_withprev <- function(n){
+    var_inv <- 1/(1/(mu_1 * n[1]) + 1/(mu_2 * n[2]))+1/sigma_prev^2
+    var <- 1/var_inv
+    se <- sqrt(var)
+    z <- beta2/se
+    power <- pnorm(z-qnorm(0.975))+pnorm(-z-qnorm(0.975))
+    return(power)
+  }
+  
+  confun_withprev <- function(n){
+    f = power_level-power_withprev(n)
+    f = rbind(f,-n[1])
+    f = rbind(f,-n[2])
+    return(list(ceq=NULL,c=f))
+  }
+  
+  objfun=function(n){
+    n[1]+n[2]
+  }
+  
+  solution_temp <- solnl(n0,objfun=objfun,confun=confun_withprev)$par
+  solution_temp_int <- round(solution_temp,0)
+  # get the integer solution around this
+  dat_para <- expand.grid(n1=c(max(solution_temp_int[1]-2,1):(solution_temp_int[1]+2)),n2=c(max(solution_temp_int[2]-2,1):(solution_temp_int[2]+2)))
+  
+  for(c in 1:nrow(dat_para)){
+    dat_para[c,3] <- power_withprev(c(dat_para$n1[c],dat_para$n2[c]))
+  }
+  
+  dat_para <- dat_para[dat_para$V3>=power_level,]
+  dat_para$n <- dat_para$n1+dat_para$n2
+  nmin <- min(dat_para$n)
+  dat_para <- dat_para[dat_para$n==nmin,]
+  dat_para <- dat_para[order(dat_para$V3,decreasing = T),]
+  return(as.numeric(dat_para[1,1:2]))
+}
+
+SolveSampleSize_Withprev_equal <- function(p_CEFTP,p_TILD_c,sigma,power_level){
+  # cal the total sample size when we added one more condition: sample sizes are equal for both treatment groups
+  beta1 = log(p_CEFTP/(1-p_CEFTP))
+  beta2 = log(p_TILD_c/(1-p_TILD_c)) - log(p_CEFTP/(1-p_CEFTP))
+  mu_1 <- exp(beta1)/(1+exp(beta1))^2
+  mu_2 <- exp(beta1 + beta2)/(1+exp(beta1 + beta2))^2
+  sigma_prev <- sigma
+  n0=c(1,1)
+  
+  power_withprev <- function(n){
+    size <- n/2
+    var_inv <- 1/(1/(mu_1 * size) + 1/(mu_2 * size))+1/sigma_prev^2
+    var <- 1/var_inv
+    se <- sqrt(var)
+    z <- beta2/se
+    power <- pnorm(z-qnorm(0.975))+pnorm(-z-qnorm(0.975))
+    return(power)
+  }
+  
+  confun_withprev <- function(n){
+    f = power_level-power_withprev(n)
+    f = rbind(f,-n)
+    return(list(ceq=NULL,c=f))
+  }
+  
+  objfun=function(n){
+    n[1]+n[2]
+  }
+  
+  solution_temp <- solnl(n0,objfun=objfun,confun=confun_withprev)$par
+  solution_temp_int <- round(solution_temp[2],0)
+  # get the even integer solution around this
+  if(solution_temp_int %% 2 ==0){
+    res <- solution_temp_int
+  }else{
+    n <- c(solution_temp_int+1,solution_temp_int-1)
+    n <- n[n>0]
+    power <- power_single(n)
+    dat_para <- data.frame(n,power)
+    dat_para <- dat_para[dat_para$power>=power_level,]
+    res <- min(dat_para$n)
+    #res <- solution_temp_int+1
+  }
+  
+  return(res)
+}
+
+SolveSampleSize_Single <- function(p_CEFTP,p_TILD_c,power_level){
+  beta1 = log(p_CEFTP/(1-p_CEFTP))
+  beta2 = log(p_TILD_c/(1-p_TILD_c)) - log(p_CEFTP/(1-p_CEFTP))
+  mu_1 <- exp(beta1)/(1+exp(beta1))^2
+  mu_2 <- exp(beta1 + beta2)/(1+exp(beta1 + beta2))^2
+  n0=c(1,1)
+  
+  power_single <- function(n){
+    var <- 1/(mu_1 * n[1]) + 1/(mu_2 * n[2])
+    se <- sqrt(var)
+    z <- beta2/se
+    power <- pnorm(z-qnorm(0.975))+pnorm(-z-qnorm(0.975))
+    return(power)
+  }
+  
+  confun_single <- function(n){
+    f = power_level-power_single(n)
+    f = rbind(f,-n[1])
+    f = rbind(f,-n[2])
+    return(list(ceq=NULL,c=f))
+  }
+  
+  objfun=function(n){
+    n[1]+n[2]
+  }
+  
+  solution_temp <- solnl(n0,objfun=objfun,confun=confun_single)$par
+  solution_temp_int <- round(solution_temp,0)
+  # get the integer solution around this
+  dat_para <- expand.grid(n1=c(max(solution_temp_int[1]-2,1):(solution_temp_int[1]+2)),n2=c(max(solution_temp_int[2]-2,1):(solution_temp_int[2]+2)))
+  
+  for(c in 1:nrow(dat_para)){
+    dat_para[c,3] <- power_single(c(dat_para$n1[c],dat_para$n2[c]))
+  }
+  
+  dat_para <- dat_para[dat_para$V3>=power_level,]
+  dat_para$n <- dat_para$n1+dat_para$n2
+  nmin <- min(dat_para$n)
+  dat_para <- dat_para[dat_para$n==nmin,]
+  dat_para <- dat_para[order(dat_para$V3,decreasing = T),]
+  return(as.numeric(dat_para[1,1:2]))
+}
+
+SolveSampleSize_Single_equal <- function(p_CEFTP,p_TILD_c,power_level){
+  beta1 = log(p_CEFTP/(1-p_CEFTP))
+  beta2 = log(p_TILD_c/(1-p_TILD_c)) - log(p_CEFTP/(1-p_CEFTP))
+  mu_1 <- exp(beta1)/(1+exp(beta1))^2
+  mu_2 <- exp(beta1 + beta2)/(1+exp(beta1 + beta2))^2
+  n0=c(1,1)
+  
+  power_single <- function(n){
+    size <- n/2
+    var <- 1/(mu_1 * size) + 1/(mu_2 * size)
+    se <- sqrt(var)
+    z <- beta2/se
+    power <- pnorm(z-qnorm(0.975))+pnorm(-z-qnorm(0.975))
+    return(power)
+  }
+  
+  confun_single <- function(n){
+    f = power_level-power_single(n)
+    f = rbind(f,-n)
+    return(list(ceq=NULL,c=f))
+  }
+  
+  objfun=function(n){
+    n[1]+n[2]
+  }
+  
+  solution_temp <- solnl(n0,objfun=objfun,confun=confun_single)$par
+  solution_temp_int <- round(solution_temp[2],0)
+  
+  # get the even integer solution around this
+  if(solution_temp_int %% 2 ==0){
+    res <- solution_temp_int
+  }else{
+    n <- c(solution_temp_int+1,solution_temp_int-1)
+    n <- n[n>0]
+    power <- power_single(n)
+    dat_para <- data.frame(n,power)
+    dat_para <- dat_para[dat_para$power>=power_level,]
+    res <- min(dat_para$n)
+    #res <- solution_temp_int+1
+  }
+  return(res)
+}
+
+# rearrange the data to the long type: one arm one study per row
+wide2long <- function(MTCdata){
+  N <- max(MTCdata$Number.of.arms)
+  study <- rep(MTCdata$Study.number,N)
+  t <- NULL
+  n <- NULL
+  r <- NULL
+  for(i in c(2,1,3)){
+    r <- c(r, eval(parse(text = paste0("MTCdata$Number.of.Event.in.arm.",i, sep = ""))))
+    n <- c(n, eval(parse(text = paste0("MTCdata$Total.number.in.arm.",i, sep = ""))))
+    t <- c(t, eval(parse(text = paste0("MTCdata$Arm.",i, sep = ""))))
+  }
+  res <- data.frame(id = study, t = t, r = r, n = n)
+  res <- res %>% dplyr::filter(!is.na(n)) %>% arrange(id)
+  res
+}
+
+# log(k1/k2)=lor
+lor2prob <- function(p1, lor){
+  p2 <- p1/(p1 + exp(lor)*(1-p1))
+  return(p2)
+}
---
+++ Rshiny/app.R
@@ -0,0 +1,343 @@
+library("netmeta")
+library("NlcOptim")
+library("kableExtra")
+library("dplyr")
+source(file = "SolveSampleSize.R")
+
+# contrast-level dat input only currently 
+ui <-
+    fluidPage(titlePanel("Sample Size Calculation to reach a predefined power for a future two-arm study (binary outcome), based
+                         on the existing network"),
+              fluidRow(
+                  
+                  column(4,style="background-color:lightgoldenrodyellow",
+                         h4("Step1: Upload the existing evidence (Contrast-level data)"),
+                         "(Please refer to the ",
+                         tags$a(href="https://github.com/fangshuye/NMA-two-arms-sample-size/blob/main/sampledat.csv", 
+                                "sample dataset"),
+                         " on Github and the ",
+                         tags$a(href="https://github.com/fangshuye/NMA-two-arms-sample-size/blob/main/README.md", 
+                                "ReadMe file"),
+                         " explains the meaning of each column)",
+                         # contrast-level dat
+                         fileInput("file1", "Choose CSV File",
+                                   multiple = F,
+                                   accept = ".csv"),
+                         helpText("Please upload the dataset using the same format",
+                                  "and column names as the sample dataset."),
+                         
+                         # Horizontal line ----
+                         tags$hr(),
+                         h4("Step2: Select two treatments in your future trial"),
+                         helpText("This part would appear after the data with correct format is uploaded"),
+                         uiOutput("treatment1"),
+                         uiOutput("treatment2")
+                    
+                  ),
+                  
+                  column(4,style="background-color:seashell",
+                         h4("Step3: Confirm the parameters"),
+                         uiOutput("treatment"), # select the treatment that you know the risk 
+                         uiOutput("Risk_above"), # enter the risk of the selected risk above
+                         htmlOutput("Risk_NMA"), # show the estimated risk of the other treatment by our existing NMA
+                         uiOutput("NMA_Enter"), # enter the risk of the other treatment if you don't want to use the estimated one
+                         uiOutput("Risk_Enter"), # the value you entered
+                         numericInput("power_level","Predefind Power",step=0.000001,value = 0.8,max = 0.99999,min = 0.1),
+                         h4("Step4: Enter the paramater to calculate the cost"),
+                         radioButtons("cost",
+                                      "Do you want to calculate the cost for each allocation plan?",
+                                      c("Yes",
+                                        "No"),
+                                      selected = "No"),
+                         uiOutput("cost_number")
+                         ),
+                  
+                  column(4,style="background-color:aliceblue",
+                         h4("Information from the existing network"),
+                         htmlOutput("txtOutput"),
+                         tags$hr(),
+                         h4("The optimal sample size for each treatment in a future trial"),
+                         tableOutput("tabOutput"),
+                         h4("Summary"),
+                         htmlOutput("Summary"))
+              ))
+
+server <- function(input, output,session) {
+    
+    #This function is repsonsible for loading in the selected file
+    filedata <- reactive({
+        infile <- input$file1
+        if (!is.null(infile)) {
+            # User has not uploaded a file yet
+            read.csv(infile$datapath)
+        }
+    })
+    
+    arm <- reactive({
+        dat <- filedata()
+        if (!is.null(dat)){
+            nma_old <- netmeta(TE,seTE,treat1,treat2,studlab,data=dat,
+                               sm="OR",comb.fixed = T,comb.random = F)
+            arms <- unique(c(dat$treat1,dat$treat2))
+            list(nma_old = nma_old, arms = arms)
+        }
+    })
+    
+    sigma_nma_old <- reactive({
+        dat <- filedata()
+        if (!is.null(dat)){
+            nma_old <- arm()$nma_old
+            trt1 <- input$choice1
+            trt2 <- input$choice2
+            nma_old$seTE.fixed[trt1,trt2]
+        }
+        
+    })
+
+    
+    output$treatment1 <- renderUI({
+        
+        dat <- filedata()
+        
+        if(!is.null(dat)){
+            radioButtons(inputId = "choice1",
+                         label = "Please select the first treatment in your future trial",
+                         choices = arm()$arms,
+                         selected = arm()$arms[2])
+            
+        }
+    })
+    
+    arm2 <- reactive({
+        dat <- filedata()
+        if (!is.null(dat)){
+            setdiff(arm()$arms,input$choice1)
+        }
+    })
+    
+    output$treatment2 <- renderUI({
+        
+        dat <- filedata()
+        
+        if(!is.null(dat)){
+            radioButtons(inputId="choice2", label="Please select the second treatment in your future trial",
+                         choices= arm2(),
+                         selected = arm2()[2])
+            
+        }
+    })
+    
+    name_trt1 <- reactive({
+        dat <- filedata()
+        if (!is.null(dat) & !is.null(input$choice1)){
+            input$choice1
+        }
+    })
+    
+    name_trt2 <- reactive({
+        dat <- filedata()
+        if (!is.null(dat) & !is.null(input$choice2)){
+            input$choice2
+        }
+    })
+    
+    output$treatment <- renderUI({
+        
+        dat <- filedata()
+        
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2)){
+            radioButtons(inputId = "baseline",
+                         label = "Please select one treatment that you know the risk",
+                         choices = c(name_trt1(),name_trt2()),
+                         selected = name_trt1())
+        }
+    })
+    
+    name_trt <- reactive({
+        dat <- filedata()
+        if (!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2)){
+            list(base = input$baseline, 
+                 second = setdiff(c(name_trt1(),name_trt2()),input$baseline))
+        }
+    })
+    
+    output$Risk_above <- renderUI({
+        
+        dat <- filedata()
+        
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2)){
+            numericInput("risk1",paste0("Risk of ",name_trt()$base),
+                         step=0.000001,value = 0.20,max = 0.99999,min = 0.00001)
+        }
+    })
+    
+    p2 <- reactive({
+        dat <- filedata()
+        if (!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2) & !is.null(input$risk1)){
+            nma_old <- arm()$nma_old
+            trt1 <- name_trt1()
+            trt2 <- name_trt2()
+            
+            # get baseline risk
+            base_trt <- name_trt()$base
+            lor_2 <- nma_old$TE.fixed[base_trt,trt2]
+            lor2prob(input$risk1,lor_2)
+        }
+        
+    })
+    
+
+    
+    output$Risk_NMA  = renderUI({
+        dat <- filedata()
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2)){
+            str1 <- paste0("The risk of ",name_trt()$second," estimated by the existing network is ",
+                           round(p2(),4))
+            HTML(str1)
+        }
+    })
+    
+    output$NMA_Enter <- renderUI({
+        
+        dat <- filedata()
+        
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2)){
+            radioButtons("howrisk",
+                         paste0("Which would you use for the risk of ",name_trt()$second),
+                         c("Enter/Define the risk" = "enter",
+                           "Use the value above directly (estimated by existing NMA)" = "NMA"),
+                         selected = "enter")
+        }
+    })
+    
+    
+    output$Risk_Enter <- renderUI({
+        dat <- filedata()
+        
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2) & !is.null(input$howrisk)){
+            if(input$howrisk=='enter'){
+                numericInput("risk2",paste0("Risk of ",name_trt()$second),
+                             step=0.000001,value = 0.25,max = 0.99999,min = 0.00001)
+            }
+        }
+    })
+
+    output$cost_number <- renderUI({
+        if(input$cost=="Yes"){
+            tagList(
+                numericInput("cost1",paste0("Cost($) per treatment(",name_trt()$base,")"),
+                             step=0.000001,value = 2,min = 0.00001),
+                numericInput("cost2",paste0("Cost($) per treatment(",name_trt()$second,")"),
+                             step=0.000001,value = 2,min = 0.00001),
+                numericInput("cost3","Cost($) per animal",
+                             step=0.000001,value = 2,min = 0.00001)
+                
+            ) 
+        }
+    })
+    
+    output$txtOutput  = renderUI({
+        dat <- filedata()
+        if(!is.null(dat)){
+            str <- paste0("Standard error of the estimated effect size 
+                          between selected two treatments by the existing network is ",
+                          round(sigma_nma_old(),4))
+          HTML(str)
+        }
+    })
+    
+    
+    SampleSize <- reactive({
+        dat <- filedata()
+        if (!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2) & !is.null(input$howrisk) & !is.null(input$risk1)){
+            sigma <- sigma_nma_old()
+            power_level <- input$power_level
+            
+            risk1 <- input$risk1
+            risk2 <- ifelse(input$howrisk=='enter',input$risk2,p2())
+            
+            samplesize_even = rep(SolveSampleSize_Withprev_equal(risk1,risk2,sigma,power_level)/2,2)
+            samplesize = SolveSampleSize_Withprev(risk1,risk2,sigma,power_level)
+            samplesize_single = SolveSampleSize_Single(risk1,risk2,power_level)
+            samplesize_single_even = rep(SolveSampleSize_Single_equal(risk1,risk2,power_level)/2,2)
+            list(NMA_even = samplesize_even,
+                 NMA = samplesize,
+                 Single_even = samplesize_single_even,
+                 Single = samplesize_single)
+        }
+        
+    })
+    
+    output$tabOutput <- function() {
+        dat <- filedata()
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2) & !is.null(input$howrisk)){
+            samplesize_even <- SampleSize()$NMA_even
+            samplesize <- SampleSize()$NMA
+            samplesize_single_even <- SampleSize()$Single_even
+            samplesize_single <- SampleSize()$Single
+            
+            output_dat <- data.frame(n1 = c(samplesize_even[1],samplesize[1],
+                                            samplesize_single_even[1],samplesize_single[1]),
+                                     n2 = c(samplesize_even[2],samplesize[2],
+                                           samplesize_single_even[2],samplesize_single[2]))
+            
+            output_dat$total <- output_dat$n1+output_dat$n2
+
+            collapse_rows_dt <- cbind(C1 = c(rep("with existing NMA", 2), rep("isolation", 2)),
+                                      C2 = c("even","uneven","even","uneven"),
+                                      output_dat)
+            colnames(collapse_rows_dt) <- c("","",name_trt()$base,name_trt()$second,"Total")
+            if(input$cost=="Yes"){
+                cost <- c(input$cost1,input$cost2,input$cost3)
+                costs <- output_dat$n1*cost[1] + output_dat$n2*cost[2] + output_dat$total*cost[3]
+                collapse_rows_dt[,6] <- costs
+                colnames(collapse_rows_dt)[1:2] <- c("","")
+                colnames(collapse_rows_dt)[6] <- "Costs ($)"
+                collapse_rows_dt %>%
+                    kbl() %>%
+                    kable_styling() %>%
+                    collapse_rows(columns = 1:2, valign = "middle")%>%
+                    add_header_above(c("","","Sample Size" = 3,"")) 
+            }else{
+                collapse_rows_dt %>%
+                    kbl() %>%
+                    kable_styling() %>%
+                    collapse_rows(columns = 1:2, valign = "middle") %>%
+                    add_header_above(c("","","Sample Size" = 3)) 
+                    
+            }
+            
+            
+        }
+    }
+    
+    output$Summary  = renderUI({
+        dat <- filedata()
+        if(!is.null(dat) & !is.null(input$choice1) & !is.null(input$choice2) & !is.null(input$howrisk) & !is.null(input$risk1)){
+            samplesize_even <- SampleSize()$NMA_even
+            samplesize <- SampleSize()$NMA
+            samplesize_single_even <- SampleSize()$Single_even
+            samplesize_single <- SampleSize()$Single
+            
+            size1 <- paste0("With existing NMA, ","the optimal sample sizes for ", 
+                            name_trt()$base," and ",name_trt()$second, " are ",
+                            samplesize[1], " and ", samplesize[2], " respectively.",
+                            " If we want to even allocate the sample size, ",
+                            "the optimal sample size for each treatment is ", 
+                            samplesize_even[1],".")
+            
+            size2 <- paste0("Without existing NMA (analyze in isolation), ","the optimal sample sizes for ", 
+                            name_trt()$base," and ",name_trt()$second, " are ",
+                            samplesize_single[1], " and ", samplesize_single[2], " respectively.",
+                            " If we want to even allocate the sample size, ",
+                            "the optimal sample size for each treatment is ", 
+                            samplesize_single_even[1],".")
+            
+            HTML(paste(size1, size2, sep = '<br/>'))
+        }
+    })
+        
+}
+
+shinyApp(ui = ui, server = server)
+
